4e36f74180fc56b375113e8dbc07595d547f9688
zhanghx04 <zhanghx04@gmail.com>
2022-08-11 02:10:47 -0500

    updated some functions

--- Book.cpp
+++ Book.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 void Book::feeddata() {
     cin.ignore();
+    Book::editdata();
+}
+
+void Book::editdata() {
     cout << "\nEnter Author Name: ";        cin.getline(m_author, 20);
     cout << "Enter Title Name: ";           cin.getline(m_title, 20);
     cout << "Enter Publisher Name:";        cin.getline(m_publisher, 20);
     cout << "Enter Price: ";                cin >> *m_price;
     cout << "Enter Stock Position: ";       cin >> *m_stock;
     cout << endl;
-}
-
-void Book::editdata() {
-
 }
 
 void Book::showdata() {
@@ -29,6 +29,20 @@ void Book::showdata() {
     cout << "Stock Position: " << *m_stock << endl;
 }
 
-int Book::search(char *, char *) {
-    return 0;
+int Book::search(char titleBuy[20], char authorBuy[20]) {
+    return strcmp(titleBuy, m_title) == 0 &&
+            strcmp(authorBuy, m_author) == 0;
+}
+
+void Book::buybook() {
+    int count;
+    cout << "Enter the Number of Books to buy: ";
+    cin >> count;
+
+    if (count <= *m_stock) {
+        *m_stock -= count;
+        cout << "Successfully Bought " << count << " Books!" << endl;
+        cout << "Total Price: $" << count * *m_price;
+    } else
+        cout << "There is not enough books in stock!";
 }
